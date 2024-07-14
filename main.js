303677d91b247c3da4cdf7efd28a5c16c3b11349
CALCULATOR <nitishjha525@gmail.com>
2024-07-14 22:00:35 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Youtuber", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
