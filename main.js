33c9a4a66c28e5e723a2b21348c9705a13f85942
CALCULATOR <nitishjha525@gmail.com>
2024-07-16 17:23:08 +0530

    add some additional transition effect

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 var typed = new Typed(".text", {
-    strings: ["Frontend Developer", "Youtuber", "Web Developer"],
+    strings: ["Fullstack Developer", "Youtuber", "Web Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
 })
+
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+};
